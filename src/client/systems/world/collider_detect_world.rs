a94aa3051f2e2e941dec5f230c28a4acfc52171a
mbayeDiao <mbayediao@mdiao.mbaye>
2025-02-01 18:40:54 +0000

    handling collision 2

--- src/client/systems/world/collider_detect_world.rs
+++ src/client/systems/world/collider_detect_world.rs
@@ -1,41 +1,41 @@
-use crate::client::components::player_component::Velocity;
 use bevy::prelude::*;
-use bevy_rapier3d::prelude::{CollisionEvent, ContactForceEvent};
+use bevy_rapier3d::prelude::{CollisionEvent, ContactForceEvent, ExternalForce, LockedAxes};
+use crate::client::components::player_component::Velocity;
+
+// pub fn collider_detect_world(
+//     mut collision_events: EventReader<CollisionEvent>,
+//     mut _query: Query<(&mut Velocity, &Transform)>
+// ) {
+//     for collision_event in collision_events.read() {
+//         match collision_event {
+//             CollisionEvent::Started(e1, e2, flags) => {
+//                 // if let Ok((mut velocity, transform, _)) = query.get_mut(*e1){}
+//                 println!("Collision started between entities {:?} and {:?} -- {:?}", e1, e2,flags);
+//             }
+//             CollisionEvent::Stopped(e1, e2, flags) => {
+//                 println!("Collision stopped between entities {:?} and {:?} -- {:?}", e1, e2,flags);
+//             }
+//         }
+//     }
+// }
 
 pub fn collider_detect_world(
     mut collision_events: EventReader<CollisionEvent>,
-    mut query: Query<(&mut Velocity, &Transform)>,
+    mut query: Query<(&mut Velocity, &mut ExternalForce, &mut LockedAxes)>,
 ) {
     for collision_event in collision_events.read() {
         match collision_event {
-            CollisionEvent::Started(e1, e2, flags) => {
-                  // Handle both entities in collision
-                  if let Ok((mut velocity1, _)) = query.get_mut(*e1) {
-                    // Stop all movement
-                    velocity1.x = 0.0;
-                    velocity1.y = 0.0;
-                    velocity1.z = 0.0;
+            CollisionEvent::Started(e1, e2, _) => {
+                if let Ok((mut velocity, mut force, mut locked_axes)) = query.get_mut(*e1) {
+                    velocity.0 = Vec3::ZERO;
+                    force.force = Vec3::ZERO;
+                    *locked_axes = LockedAxes::TRANSLATION_LOCKED;
                 }
-                
-                 println!("Collision started between entities {:?} and {:?} -- {:?}", e1, e2,flags);
             }
-            CollisionEvent::Stopped(e1, e2, flags) => {
-                
-                // Reset velocity when collision stops
-                if let Ok((mut velocity, _)) = query.get_mut(*e1) {
-                    velocity.x = 0.;
-                    velocity.y = 0.;
-                    velocity.z = 0.;
+            CollisionEvent::Stopped(e1, _, _) => {
+                if let Ok((_, _, mut locked_axes)) = query.get_mut(*e1) {
+                    *locked_axes = LockedAxes::ROTATION_LOCKED; // Re-enable movement if needed
                 }
-                if let Ok((mut velocity, _)) = query.get_mut(*e2) {
-                    velocity.x = 0.;
-                    velocity.y = 0.;
-                    velocity.z = 0.;
-                }
-                println!(
-                    "Collision stopped between entities {:?} and {:?} -- {:?}",
-                    e1, e2, flags
-                );
             }
         }
     }
